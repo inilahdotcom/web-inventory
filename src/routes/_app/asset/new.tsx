daf7df580c8fb1216ebdcb4285f4a843df6f1389
Dzaki21 <dzakiali4@gmail.com>
2026-08-12 08:52:25 +0700

    feat(UI): Add page Audit-login

--- src/routes/_app/asset/new.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { AssetCreateView } from '@/features/assets/components/AssetCreateView'
-
-export const Route = createFileRoute('/_app/asset/new')({
-  component: AssetCreateView,
-})
--- src/routes/_auth/login.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { LoginView } from '@/features/auth/components/LoginView'
-
-export const Route = createFileRoute('/_auth/login')({
-  component: LoginView,
-})
