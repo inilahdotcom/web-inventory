45294f5f63920ac10eb49b8f1dede8eb25d288aa
Zernnx <daffataufiqurahman31@gmail.com>
2026-08-24 02:28:01 +0700

    feat: add user management view

---
+++ src/routes/_app/pengguna.tsx
@@ -0,0 +1,6 @@
+import { createFileRoute } from "@tanstack/react-router"
+import { UserManagementView } from "@/features/users/components/UserManagementView"
+
+export const Route = createFileRoute("/_app/pengguna")({
+  component: UserManagementView,
+})
